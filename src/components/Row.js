6741c068dd73ec2e3b16c7032af41570fdbf0bc4
iamdanielchukwuma@gmail.com <iamdanielchukwuma@gmail.com>
2022-05-03 14:33:14 +0100

    Output the guess on the browser

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
 	if (guess) {
 		return (
 			<div className="row pass">
@@ -8,6 +8,25 @@ export default function Row({ guess }) {
 					<div key={i} className={l.color}>
 						{l.key}
 					</div>
+				))}
+			</div>
+		);
+	}
+
+	if (currentGuess) {
+		/** Convert the string to array */
+		let letters = currentGuess.split("");
+
+		return (
+			/** To display the letter on the browser */
+			<div className="row current">
+				{letters.map((letter, i) => (
+					<div key={i} className="field">
+						{letter}
+					</div>
+				))}
+				{[...Array(5 - letters.length)].map((_, i) => (
+					<div key={i}></div>
 				))}
 			</div>
 		);
